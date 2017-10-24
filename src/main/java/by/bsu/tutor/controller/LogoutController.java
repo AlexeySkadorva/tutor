9d4395201bc8411afcad5e24584d1580d6035706
Alexey Skadorva <leshaskadorva@gmail.com>
2017-10-24 08:52:03 +0300

    Added login and logout links to menu

--- src/main/java/by/bsu/tutor/controller/LogoutController.java
+++ src/main/java/by/bsu/tutor/controller/LogoutController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
-public class LogoutController {
-
-    @RequestMapping(value = "/logoutDialog")
-    public String logoutDialog(Model model, HttpServletRequest request) {
-        ControllerUtil.setReferrer(request, model);
-        return "logout";
-    }
-
-}
+//@Controller
+//public class LogoutController {
+//
+//    @RequestMapping(value = "/logout")
+//    public String logoutDialog(Model model, HttpServletRequest request) {
+//        ControllerUtil.setReferrer(request, model);
+//        return "logout";
+//    }
+//
+//}
